f99bf12ebb6675da69292a2c4847d7c01cb423cc
Sebastian Nicolas <sebastianNicolas@no-reply.com>
2025-02-15 11:19:35 -0500

    initial

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "cyberdream",
+    },
+  },
+}
